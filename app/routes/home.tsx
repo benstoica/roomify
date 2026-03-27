7de1952b2743014acfeaab48839b8ed80f5a997b
Ben Stoica <bennystoica@gmail.com>
2026-03-27 11:48:59 -0400

    added puter workers, 3d floor plan, and project creation

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,9 +3,9 @@ import type { Route } from "./+types/home";
 import { ArrowRight, ArrowUpRight, Clock, Layers } from "lucide-react";
 import Button from "components/ui/Button";
 import Upload from "components/Upload";
-import { useNavigate } from "react-router";
-import { useState } from "react";
-import { createProject } from "lib/puter.action";
+import { Link, useNavigate } from "react-router";
+import { useEffect, useRef, useState } from "react";
+import { createProject, getProjects } from "lib/puter.action";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -17,39 +17,60 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   const navigate = useNavigate();
   const [projects, setProjects] = useState<DesignItem[]>([]);
+  const isCreatingProjectRef = useRef(false);
 
   const handleUploadComplete = async (base64Image: string) => {
-    const newId = Date.now().toString();
-    const name = `Residence ${newId}`;
+    try {
+      if (isCreatingProjectRef.current) {
+        return false;
+      }
+      isCreatingProjectRef.current = true;
+      const newId = Date.now().toString();
+      const name = `Residence ${newId}`;
 
-    const newItem = {
-      id: newId,
-      name,
-      sourceImage: base64Image,
-      renderedImage: undefined,
-      timestamp: Date.now(),
+      const newItem = {
+        id: newId,
+        name,
+        sourceImage: base64Image,
+        renderedImage: undefined,
+        timestamp: Date.now(),
+      };
+
+      const saved = await createProject({
+        item: newItem,
+        visibility: "private",
+      });
+
+      if (!saved) {
+        console.error("Failed to create project");
+        return;
+      }
+
+      setProjects((prev) => [saved, ...prev]);
+
+      navigate(`/visualizer/${saved.id}`, {
+        state: {
+          initialImage: saved.sourceImage,
+          initialRendered: saved.renderedImage ?? null,
+          name: saved.name,
+        },
+      });
+
+      return true;
+    } finally {
+      isCreatingProjectRef.current = false;
+    }
+  };
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const items = await getProjects();
+
+      setProjects(items);
     };
 
-    const saved = await createProject({
-      item: newItem,
-      visibility: "private",
-    });
-
-    if (!saved) {
-      console.error("Failed to create project");
-      return;
-    }
-
-    setProjects((prev) => [saved, ...prev]);
-
-    navigate(`/visualizer/${saved.id}`, {
-      state: {
-        initialImage: saved.sourceImage,
-        initialRendered: saved.renderedImage ?? null,
-        name: saved.name,
-      },
-    });
-  };
+    fetchProjects();
+  });
 
   return (
     <div className="home">
@@ -111,28 +132,32 @@ export default function Home() {
           <div className="projects-grid">
             {projects.map(
               ({ id, name, renderedImage, sourceImage, timestamp }) => (
-                <div className="project-card group" key={id}>
-                  <div className="preview">
-                    <img src={renderedImage ?? sourceImage} alt="project" />
-                    <div className="badge">
-                      <span>Community</span>
-                    </div>
-                  </div>
-                  <div className="card-body">
-                    <div>
-                      <h3>{name}</h3>
-                      <div className="meta">
-                        <Clock size={12} />
-                        <span>{new Date(timestamp).toLocaleDateString()}</span>
-                        <span>By Ben Stoica</span>
+                <Link key={id} to={`/visualizer/${id}`}>
+                  <div className="project-card group">
+                    <div className="preview">
+                      <img src={renderedImage ?? sourceImage} alt="project" />
+                      <div className="badge">
+                        <span>Community</span>
                       </div>
                     </div>
+                    <div className="card-body">
+                      <div>
+                        <h3>{name}</h3>
+                        <div className="meta">
+                          <Clock size={12} />
+                          <span>
+                            {new Date(timestamp).toLocaleDateString()}
+                          </span>
+                          <span>By Ben Stoica</span>
+                        </div>
+                      </div>
 
-                    <div className="arrow">
-                      <ArrowUpRight size={18} />
+                      <div className="arrow">
+                        <ArrowUpRight size={18} />
+                      </div>
                     </div>
                   </div>
-                </div>
+                </Link>
               ),
             )}
           </div>
